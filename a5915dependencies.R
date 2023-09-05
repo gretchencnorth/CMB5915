5eba9827baa27d51032df015b687715d8fc74fd6
gretchencnorth <126915933+gretchencnorth@users.noreply.github.com>
2023-09-05 14:50:09 -0500

    Add files via upload
    Homework 1 submission

---
+++ a5915dependencies.R
@@ -0,0 +1,36 @@
+install.packages("tidyverse")
+install.packages(c("shiny", 
+                   "learnr", 
+                   "openintro", 
+                   "mdsr", 
+                   "quarto", 
+                   "knitr", 
+                   "pandoc"))
+install.packages(c("ggbeeswarm", 
+                   "emmeans", 
+                   "car", 
+                   "patchwork", 
+                   "multcompView", 
+                   "writexl", 
+                   "binom"))
+update.packages(ask = FALSE)
+
+  library(tidyverse)
+  library(shiny)
+  library(learnr)
+  library(openintro) # new
+  library(mdsr) # new
+  library(quarto)
+  library(knitr)
+  library(pandoc) # new
+  library(openintro) # new
+  library(ggbeeswarm) # new
+  library(emmeans) # new
+  library(car)
+  library(patchwork) # new
+  library(multcompView) # new
+  library(writexl) # new
+  library(binom)
+
+
+
